f08038c17c021a07150962dccbd65800b575a949
Anjasoa1 <124576055+Anjasoa1@users.noreply.github.com>
2025-04-01 06:04:25 +0200

    TP SPRING
    commit spring project

--- src/main/java/com/example/annoncesapi/model/Annonce.java
+++ src/main/java/com/example/annoncesapi/model/Annonce.java
@@ -21,7 +21,6 @@ public class Annonce {
     @Column(updatable = false)
     private Timestamp date;
 
-    // Les Getter et Setter
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
 
